3d9c585a41ec5872ab5f8bbf918212766d32fef8
Johan Ramirez <johand200505@gmail.com>
2026-06-18 15:41:28 -0400

    Added a check whether user input is 12 digits or not

--- Lab8_jramirez47-1.py
+++ Lab8_jramirez47-1.py
@@ -4,8 +4,13 @@ Johan D. Ramirez Maldonado
 This program will validate UPC codes
 06/18/2026
 """
-def find_UPC(*arg):
-    return arg[0]
+def find_UPC(code):
+    print("This is a placeholder for now")
 
-ask_user = input("Enter a UPC code: ")
-#find_UPC(ask_user)
+
+
+ask_user = input("Enter a UPC code (must be 12 digits): ")
+while len(ask_user) != 12:
+    print("Invalid.")
+    ask_user = input("Enter a UPC code (must be 12 digits): ")
+find_UPC(ask_user)
