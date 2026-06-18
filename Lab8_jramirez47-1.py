1390a1f7256881d78c5b431b40c04223c1828aa9
Johan Ramirez <johand200505@gmail.com>
2026-06-18 15:20:08 -0400

    Added function "find_UPC" and varaible that takes in user input

--- Lab8_jramirez47-1.py
+++ Lab8_jramirez47-1.py
@@ -4,3 +4,8 @@ Johan D. Ramirez Maldonado
 This program will validate UPC codes
 06/18/2026
 """
+def find_UPC(*arg):
+    return arg[0]
+
+ask_user = input("Enter a UPC code: ")
+#find_UPC(ask_user)
